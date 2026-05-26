0801788c45897488e5ccf992f3f23ea44ac0b496
Nicolas <nicfenato@gmail.com>
2026-05-26 07:58:51 -0300

    Atualizações no model (incompleto)

--- backend/src/main/java/myMenu/backend/demo/model/Usuario.java
+++ backend/src/main/java/myMenu/backend/demo/model/Usuario.java
@@ -1,11 +1,15 @@
 package myMenu.backend.demo.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "usuario")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Usuario {
 
     @Id
@@ -49,9 +53,6 @@ public class Usuario {
     @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
     private List<CardapioSemanal> cardapios = new ArrayList<>();
 
-    public Usuario() {
-    }
-
     public Usuario(String nome, String email) {
         this.nome = nome;
         this.email = email;
@@ -67,68 +68,5 @@ public class Usuario {
             this.email = novoEmail;
         }
     }
-
-    public Long getId() { 
-        return id; 
-    }
-
-    public void setId(Long id) { 
-        this.id = id; 
-    }
-
-    public String getNome() { 
-        return nome; 
-    }
-
-    public void setNome(String nome) { 
-        this.nome = nome; 
-    }
-
-    public String getEmail() { 
-        return email; 
-    }
-
-    public void setEmail(String email) { 
-        this.email = email; 
-    }
-
-    public List<String> getPreferenciasAlimentares() { 
-        return preferenciasAlimentares; 
-    }
-
-    public void setPreferenciasAlimentares(List<String> p) { 
-        this.preferenciasAlimentares = p; 
-    }
-
-    public List<String> getRestricoes() { 
-        return restricoes; 
-    }
-
-    public void setRestricoes(List<String> restricoes) { 
-        this.restricoes = restricoes; 
-    }
-
-    public List<Receita> getFavoritas() { 
-        return favoritas; 
-    }
-
-    public void setFavoritas(List<Receita> favoritas) { 
-        this.favoritas = favoritas; 
-    }
-
-    public Geladeira getGeladeira() { 
-        return geladeira; 
-    }
-
-    public void setGeladeira(Geladeira geladeira) { 
-        this.geladeira = geladeira; 
-    }
-
-    public List<CardapioSemanal> getCardapios() { 
-        return cardapios; 
-    }
-
-    public void setCardapios(List<CardapioSemanal> cardapios) { 
-        this.cardapios = cardapios; 
-    }
+    
 }
